61a3fcc20bb83c7e951eaf669f422d99138e3c52
ProblemTryer <honglonghelong@163.com>
2021-03-17 23:53:55 -0400

    version 0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,4 +9,19 @@ class App extends Component{
         )
     }
 }
-export default App;
+
+class TabBar extends Component{
+    render(){
+        return (
+            <div class="level-mid">
+            <p class="level-item"><strong>All</strong></p>
+            <p class="level-item"><a href="">About</a></p>
+            <p class="level-item"><a href="">Help</a></p>
+            <p class="level-item"><a href="">Features</a></p>
+            <p class="level-item"><a href="" class="button is-success">Sign-up</a></p>
+          </div>
+        )
+    }
+}
+
+export {App, TabBar};
